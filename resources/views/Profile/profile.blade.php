9436ad2832419297373233b35d80b9d9e7ed8c1b
mriganko07 <mrigankaadhikary35@gmail.com>
2024-04-19 17:06:16 +0530

    admin profile change

--- resources/views/Profile/profile.blade.php
+++ resources/views/Profile/profile.blade.php
@@ -71,10 +71,10 @@
                         @endif
                         <a href="#" class="btn">Edit </a>
                     </div>
-                    <p1 class="copyright">
+                    <span class="copyright">
                         &copy; 2024 FoodHub. All Rights Reserved | Crafted by <a href="https://www.facebook.com/sanket.adhikary.7?mibextid=uzlsIk"
                         target="_blank" class="link">Sanket Adhikary</a>
-                    </p1>
+                    </span>
                 </div>
 
 
