115ccb4ae78b814694cfcf35499c0a2a9c780976
mriganko07 <mrigankaadhikary35@gmail.com>
2024-04-25 12:58:55 +0530

    review animation & booktable done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -786,29 +786,136 @@
 
       <section class="section testi text-center has-bg-image"
         style="background-image: url('images/testimonial-bg.jpg')" aria-label="testimonials">
-        <div class="container">
 
-          <div class="quote">”</div>
+        <div class="inner">
 
-          <p class="headline-2 testi-text">
-            I wanted to thank you for inviting me down for that amazing dinner the other night. The food was
-            extraordinary.
-          </p>
+          <div class="container_wrapper">
 
-          <div class="wrapper">
-            <div class="separator"></div>
-            <div class="separator"></div>
-            <div class="separator"></div>
-          </div>
-
-          <div class="profile">
-            <img src="images/testi-avatar.jpg" width="100" height="100" loading="lazy" alt="Sam Raimi"
-              class="img">
-
-            <p class="label-2 profile-name">Sam Raimi</p>
+            <div class="container_test">
+    
+              <div class="quote">”</div>
+    
+              <p class="headline-2 testi-text">
+                I wanted to thank you for inviting me down for that amazing dinner the other night. The food was
+                extraordinary.
+              </p>
+    
+              <div class="wrapper">
+                <div class="separator"></div>
+                <div class="separator"></div>
+                <div class="separator"></div>
+              </div>
+    
+              <div class="profile">
+                <img src="images/testi-avatar.jpg" width="100" height="100" loading="lazy" alt="Sam Raimi"
+                  class="img">
+    
+                <p class="label-2 profile-name">Sam Raimi</p>
+              </div>
+    
+            </div>
+  
+            <div class="container_test">
+    
+              <div class="quote">”</div>
+    
+              <p class="headline-2 testi-text">
+                I wanted to thank you for inviting me down for that amazing dinner the other night. The food was
+                extraordinary.
+              </p>
+    
+              <div class="wrapper">
+                <div class="separator"></div>
+                <div class="separator"></div>
+                <div class="separator"></div>
+              </div>
+    
+              <div class="profile">
+                <img src="images/testi-avatar.jpg" width="100" height="100" loading="lazy" alt="Sam Raimi"
+                  class="img">
+    
+                <p class="label-2 profile-name">Sam Raimi</p>
+              </div>
+    
+            </div>
+  
+            <div class="container_test">
+    
+              <div class="quote">”</div>
+    
+              <p class="headline-2 testi-text">
+                I wanted to thank you for inviting me down for that amazing dinner the other night. The food was
+                extraordinary.
+              </p>
+    
+              <div class="wrapper">
+                <div class="separator"></div>
+                <div class="separator"></div>
+                <div class="separator"></div>
+              </div>
+    
+              <div class="profile">
+                <img src="images/testi-avatar.jpg" width="100" height="100" loading="lazy" alt="Sam Raimi"
+                  class="img">
+    
+                <p class="label-2 profile-name">Sam Raimi</p>
+              </div>
+    
+            </div>
+  
+            <div class="container_test">
+    
+              <div class="quote">”</div>
+    
+              <p class="headline-2 testi-text">
+                I wanted to thank you for inviting me down for that amazing dinner the other night. The food was
+                extraordinary.
+              </p>
+    
+              <div class="wrapper">
+                <div class="separator"></div>
+                <div class="separator"></div>
+                <div class="separator"></div>
+              </div>
+    
+              <div class="profile">
+                <img src="images/testi-avatar.jpg" width="100" height="100" loading="lazy" alt="Sam Raimi"
+                  class="img">
+    
+                <p class="label-2 profile-name">Sam Raimi</p>
+              </div>
+    
+            </div>
+  
+            <div class="container_test">
+    
+              <div class="quote">”</div>
+    
+              <p class="headline-2 testi-text">
+                I wanted to thank you for inviting me down for that amazing dinner the other night. The food was
+                extraordinary.
+              </p>
+    
+              <div class="wrapper">
+                <div class="separator"></div>
+                <div class="separator"></div>
+                <div class="separator"></div>
+              </div>
+    
+              <div class="profile">
+                <img src="images/testi-avatar.jpg" width="100" height="100" loading="lazy" alt="Sam Raimi"
+                  class="img">
+    
+                <p class="label-2 profile-name">Sam Raimi</p>
+              </div>
+    
+            </div>
+            
           </div>
 
         </div>
+
+
       </section>
 
 
